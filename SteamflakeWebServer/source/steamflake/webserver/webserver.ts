08aefe69b7c8fad4be3a98c4e88f09283783288a
Martin Nordberg <mnordberg@new-host-2.home>
2014-06-18 05:46:15 -0400

    Worked on REST web server.

--- SteamflakeWebServer/source/steamflake/webserver/webserver.ts
+++ SteamflakeWebServer/source/steamflake/webserver/webserver.ts
@@ -13,6 +13,12 @@ var nstatic = require( 'node-static' );
 
 import restify = require( 'restify' );
 
+import elements = require( '../../../../SteamflakeCore/source/steamflake/core/metamodel/elements' );
+import jsonfilepersistence = require( '../../../source/steamflake/webserver/jsonfilepersistence' );
+import persistence = require( '../../../../SteamflakeCore/source/steamflake/core/metamodel/persistence' );
+import structure = require( '../../../../SteamflakeModel/source/steamflake/model/structure' );
+import uuids = require( '../../../../SteamflakeCore/source/steamflake/core/utilities/uuids' );
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
@@ -42,11 +48,19 @@ class SteamflakeWebServer
      */
     public serve() {
 
+        var me = this;
+
+        // set up persistence store
+        me._store = jsonfilepersistence.makeJsonFilePersistentStore( SteamflakeWebServer.WORKSPACE_PATH + "/tmp/model" );
+
+        // create an initial model
+        me.buildModel();
+
         // Restify server name
         var serverName = "SteamflakeWebServer";
 
         // Logger
-        var logger = bunyan.createLogger( {
+        me._logger = bunyan.createLogger( {
             name: serverName,
             streams: [
                 {
@@ -57,24 +71,80 @@ class SteamflakeWebServer
         } );
 
         // Create the Restify server
-        var server = restify.createServer( {
+        me._server = restify.createServer( {
             name: serverName,
-            log: logger
+            log: me._logger
         } );
 
         // set up static content
-        this.serveStaticContent( server );
+        me.serveStaticContent();
+
+        // root packages
+        me.serveRootPackage();
 
         // Establish request logging
-        server.on( 'after', restify.auditLogger({
-            log: logger
-        }));
+        me.audit();
 
         // Start the server
-        server.listen( 8080, function() {
-            console.log('Server started: %s listening at %s.', server.name, server.url);
-            logger.info('Server started: %s listening at %s.', server.name, server.url);
+        me.listen();
+
+    }
+
+    /**
+     * Sets up HTTP request audit logging.
+     */
+    private audit() {
+        this._server.on( 'after', restify.auditLogger({
+            log: this._logger
+        }));
+    }
+
+    /**
+     * Creates a sample model for experimentation.
+     */
+    private buildModel() {
+        var rootElement = structure.makeRootPackage( uuids.makeUuid() );
+
+        this._store.creator.createModelElement( rootElement );
+    }
+
+    /**
+     * Starts the server listening.
+     */
+    private listen() {
+        var me = this;
+
+        me._server.listen( 8080, function() {
+            console.log( 'Server started: %s listening at %s.', me._server.name, me._server.url );
+            me._logger.info( 'Server started: %s listening at %s.', me._server.name, me._server.url );
         });
+    }
+
+    /**
+     * Serves JSON for the root package.
+     */
+    private serveRootPackage() {
+
+        var me = this;
+
+        // RootPackage response
+        me._server.get(
+            '/Steamflake/RootPackage',
+            function respond( req: restify.Request, res: restify.Response, next: Function ) {
+
+                var load = function() {
+                    return me._store.reader.loadRootModelElement();
+                };
+
+                var returnJson = function( modelElement : structure.IRootPackage ) {
+                    res.json( modelElement.toJson( elements.EJsonDetailLevel.Attributes ) );
+                    next();
+                };
+
+                load().then( returnJson );
+            }
+        );
+
 
     }
 
@@ -82,15 +152,15 @@ class SteamflakeWebServer
      * Sets up the server to serve static content.
      * @param server The server under construction.
      */
-    private serveStaticContent( server : restify.Server ) {
+    private serveStaticContent() {
 
         // Static file server
-        var file = new nstatic.Server( this.WORKSPACE_PATH );
+        var file = new nstatic.Server( SteamflakeWebServer.WORKSPACE_PATH );
 
         // Serve simple static web files;
         // NOTE: Main page = http://localhost:8080/Steamflake/SteamflakeWebClient/index.html
         // TBD: Secure non-production resources
-        server.get( /^\/Steamflake\/.*\.(html|js|css|png|eot|svg|ttf|woff)/, function( req, res, next ) {
+        this._server.get( /^\/Steamflake\/.*\.(html|js|css|png|eot|svg|ttf|woff)/, function( req, res, next ) {
             file.serve( req, res, function ( err ) {
                 if ( err ) {
                     throw err;
@@ -101,8 +171,17 @@ class SteamflakeWebServer
 
     }
 
+    /** Bunyan logger */
+    private _logger : any/*TBD: need .d.ts for bunyan*/;
+
+    /** The HTTP server. */
+    private _server : restify.Server;
+
+    /** The persistent store for Steamflake model elements. */
+    private _store : persistence.IPersistentStore<structure.IRootPackage>;
+
     // File system configuration. TBD:  Make configurable.
-    private WORKSPACE_PATH = '/home/mnordberg/workspace';
+    private static WORKSPACE_PATH = '/home/mnordberg/workspace';
 
 }
 
